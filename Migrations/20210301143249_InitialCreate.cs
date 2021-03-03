181ce3d3d385fa0ae57efbe640380fd0c26cf385
Emanoel Abrahão Albuquerque Barros <abrahaoab.ti@gmail.com>
2021-03-03 09:38:13 -0300

    Adicionar arquivos de projeto.

---
+++ Migrations/20210301143249_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CursoMVC.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up([NotNull] MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(name: "SqlServer:Identity", value: "1, 1"),
+                    Descricao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey(name: "PK_Categorias", columns: x => x.Id);
+                });
+        }
+
+        protected override void Down([NotNull] MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
